cee0deacc299768c81f771f833d13f3abf482ec9
netsoda2757 <bohndev000@pointschools.net>
2025-09-16 16:06:23 -0500

    Testing

--- static/assets/js/007.js
+++ static/assets/js/007.js
@@ -1,11 +1,6 @@
 // t.js
 window.addEventListener("load", () => {
   navigator.serviceWorker.register("../sw.js?v=2025-04-15", { scope: "/a/" });
-  setTimeout(() => {
-    if (typeof popout === "function") {
-      popout();
-    }
-  }, 1000);
   const form = document.getElementById("fv");
   const input = document.getElementById("iv");
   if (form && input) {
